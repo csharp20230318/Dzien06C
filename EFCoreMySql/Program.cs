33410b50a2c0eb45ac0a5b3b108a00d43b9405c2
kurs <kurs@DESKTOP-V3HDKPQ>
2023-04-23 15:35:26 +0200

    Add project files.

---
+++ EFCoreMySql/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+Console.WriteLine("Hello, World!");
+
+public class BlogDataContext : DbContext
+{
+    static readonly string connectionString = "server=localhost;port=3306;user=root;password=root;database=blog;";
+
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
+
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public Author Author { get; set; }
+
+}
+
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public List<Post>? Posts { get; set; }
+}
